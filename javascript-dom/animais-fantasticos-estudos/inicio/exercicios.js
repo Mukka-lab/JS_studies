cc0b153d25582d6a1514ef6ec2222e350bf3b7c8
Gabriel Muquinho <gmuquinho@gmail.com>
2022-05-31 15:32:58 -0300

    dia 30/05/2022 finalizado

---
+++ javascript-dom/animais-fantasticos-estudos/inicio/exercicios.js
@@ -0,0 +1,19 @@
+// Retorne o url da página atual utilizando o objeto window
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+let primeiroElemento = document.querySelector('.ativo');
+
+
+console.log(primeiroElemento);
+
+
+// Retorne a linguagem do navegador
+
+// Retorne a largura da janela
+
+let larguraJanela = window.innerWidth;
+
+
+console.log(larguraJanela);
